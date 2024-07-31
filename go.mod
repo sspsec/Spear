6103f04c3bf80af6b9ea3edd4be900650938fe8b
SSPSec <142762749+sspsec@users.noreply.github.com>
2024-07-31 22:29:49 +0800

    Add files via upload

--- go.mod
+++ go.mod
@@ -1,84 +1,45 @@
-module spear
+module Spear
 
 go 1.22
 
 require (
-	fyne.io/fyne/v2 v2.4.5
+	fyne.io/fyne/v2 v2.5.0
+	github.com/CodyGuo/win v0.0.0-20170113125346-08e6b7208274
 	github.com/lengzhao/font v0.1.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	dmitri.shuralyov.com/font/woff2 v0.0.0-20180220214647-957792cbbdab // indirect
-	fyne.io/systray v1.10.1-0.20231115130155-104f5ef7839e // indirect
-	github.com/BurntSushi/toml v1.3.2 // indirect
+	fyne.io/systray v1.11.0 // indirect
+	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/ConradIrwin/font v0.0.0-20210318200717-ce8d41cc0732 // indirect
-	github.com/Kodeworks/golang-image-ico v0.0.0-20141118225523-73f0f4cfade9 // indirect
-	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/aws/aws-sdk-go v1.51.13 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
-	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/flopp/go-findfont v0.1.0 // indirect
-	github.com/fredbi/uri v1.0.0 // indirect
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/fyne-io/fyne-cross v1.5.0 // indirect
+	github.com/fredbi/uri v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
-	github.com/fyne-io/glfw-js v0.0.0-20220120001248-ee7290d23504 // indirect
+	github.com/fyne-io/glfw-js v0.0.0-20240101223322-6e1efdc71b7a // indirect
 	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
-	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240306074159-ea2d69986ecb // indirect
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
 	github.com/go-text/render v0.1.0 // indirect
 	github.com/go-text/typesetting v0.1.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/googleapis/gnostic v0.1.0 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
-	github.com/imdario/mergo v0.3.5 // indirect
-	github.com/jeandeaual/go-locale v0.0.0-20220711133428-7de61946b173 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/jeandeaual/go-locale v0.0.0-20240223122105-ce5225dcaa49 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
-	github.com/klauspost/compress v1.13.4 // indirect
-	github.com/klauspost/pgzip v1.2.5 // indirect
-	github.com/mholt/archiver/v3 v3.5.1 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/nwaples/rardecode v1.1.0 // indirect
-	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/nicksnyder/go-i18n/v2 v2.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/rymdport/portal v0.2.2 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	github.com/tevino/abool v1.2.0 // indirect
-	github.com/ulikunitz/xz v0.5.10 // indirect
-	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
-	github.com/yuin/goldmark v1.5.5 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/image v0.11.0 // indirect
-	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
-	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
-	k8s.io/api v0.18.19 // indirect
-	k8s.io/apimachinery v0.18.19 // indirect
-	k8s.io/client-go v0.18.17 // indirect
-	k8s.io/klog v1.0.0 // indirect
-	k8s.io/kubectl v0.18.17 // indirect
-	k8s.io/utils v0.0.0-20200324210504-a9aa75ae1b89 // indirect
-	sigs.k8s.io/structured-merge-diff/v3 v3.0.1 // indirect
-	sigs.k8s.io/yaml v1.2.0 // indirect
+	github.com/yuin/goldmark v1.7.1 // indirect
+	golang.org/x/image v0.18.0 // indirect
+	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	_ "github.com/lengzhao/font/autoload"
@@ -18,114 +19,131 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
-	"syscall"
+	"time"
 )
 
 type customTheme struct {
 	fyne.Theme
+	isDarkMode bool
+}
+
+func (t customTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	switch name {
+	case theme.ColorNameBackground:
+		if t.isDarkMode {
+			return color.RGBA{R: 28, G: 28, B: 30, A: 255}
+		}
+		return color.White
+	case theme.ColorNameButton:
+		if t.isDarkMode {
+			return color.RGBA{R: 72, G: 72, B: 74, A: 255}
+		}
+		return color.RGBA{R: 212, G: 227, B: 250, A: 255}
+	case theme.ColorNameDisabledButton:
+		if t.isDarkMode {
+			return color.RGBA{R: 58, G: 58, B: 60, A: 255}
+		}
+		return color.RGBA{R: 174, G: 174, B: 178, A: 255}
+	case theme.ColorNameForeground:
+		if t.isDarkMode {
+			return color.White
+		}
+		return color.Black
+	case theme.ColorNameHover:
+		if t.isDarkMode {
+			return color.RGBA{R: 44, G: 44, B: 46, A: 255}
+		}
+		return color.RGBA{R: 230, G: 230, B: 230, A: 255}
+	case theme.ColorNameInputBackground:
+		if t.isDarkMode {
+			return color.RGBA{R: 44, G: 44, B: 46, A: 255}
+		}
+		return color.White
+	default:
+		return t.Theme.Color(name, variant)
+	}
 }
 
 type Config struct {
-	Java_path struct {
+	JavaPath struct {
 		Java8  string `yaml:"Java8"`
 		Java11 string `yaml:"Java11"`
 		Open   string `yaml:"Open"`
 	} `yaml:"javapath"`
 }
 
-type Categories struct {
-	Category []struct {
-		Name string `yaml:"CategoryName"`
-		Tool []struct {
-			Name     string `yaml:"ToolName"`
-			PATH     string `yaml:"PATH"`
-			FileName string `yaml:"FileName"`
-			VALUE    string `yaml:"VALUE"`
-			COMMAND  string `yaml:"COMMAND"`
-			Optional string `yaml:"Optional"`
-		} `yaml:"Tools"`
-	} `yaml:"Categories"`
+type Tool struct {
+	Name     string `yaml:"ToolName"`
+	Path     string `yaml:"PATH"`
+	FileName string `yaml:"FileName"`
+	Value    string `yaml:"VALUE"`
+	Command  string `yaml:"COMMAND"`
+	Optional string `yaml:"Optional"`
 }
 
-func (t customTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
-	switch name {
-	case theme.ColorNameForeground:
-		return color.Black // 设置文字颜色为纯黑以增强对比度和可读性
-	case theme.ColorNameButton:
-		return color.White
-	case theme.ColorNameBackground:
-		return color.RGBA{R: 173, G: 216, B: 230, A: 255} // 清新一点的颜色
-	default:
-		return t.Theme.Color(name, variant)
-	}
+type Category struct {
+	Name string `yaml:"CategoryName"`
+	Tool []Tool `yaml:"Tools"`
 }
+
+type Categories struct {
+	Category []Category `yaml:"Categories"`
+}
+
+var categories Categories
+var config Config
+var cachedYAMLData []byte
 
 func main() {
 	myApp := app.NewWithID("com.sspsec.Spear")
+	currentTheme := &customTheme{Theme: theme.LightTheme(), isDarkMode: false}
 
-	myApp.Settings().SetTheme(customTheme{theme.LightTheme()})
-	myWindow := myApp.NewWindow("SSP渗透集成工具箱V2_by_Spe4r 公众号:SSP安全研究")
+	if fyne.CurrentApp().Settings().ThemeVariant() == theme.VariantDark {
+		currentTheme.isDarkMode = true
+		currentTheme.Theme = theme.DarkTheme()
+	} else {
+		currentTheme.isDarkMode = false
+		currentTheme.Theme = theme.LightTheme()
+	}
+	myApp.Settings().SetTheme(currentTheme)
 
-	background := canvas.NewRectangle(color.RGBA{R: 173, G: 216, B: 230, A: 255}) // 设置清新背景颜色
+	myWindow := myApp.NewWindow("SSP渗透集成工具箱V3_by_Spe4r 公众号:SSP安全研究")
+	myWindow.SetMaster()
 
 	outputLabel := widget.NewLabel("Output will be shown here")
 	outputLabel.Wrapping = fyne.TextWrapBreak
 
-	var categories Categories
 	yamldata, err := ReadYAMLFile()
 	if err != nil {
 		log.Fatal(err)
 	}
 	yaml.Unmarshal(yamldata, &categories)
+	yaml.Unmarshal(yamldata, &config)
 
-	var canvasObjects []fyne.CanvasObject
-	var allButtons []*widget.Button
 	var categoryContainers []fyne.CanvasObject
+	var allContainers []*fyne.Container
 
-	for _, category := range categories.Category {
-		label := widget.NewLabel(category.Name)
-		labelContainer := container.NewMax(label)
-		var buttons []fyne.CanvasObject
+	updateToolContainers(outputLabel, &categoryContainers, &allContainers)
 
-		for _, toolbase := range category.Tool {
-			toolbase := toolbase // 在循环内创建当前迭代的副本
-			btn := widget.NewButton(toolbase.Name, func() {
-				err := ExecuteCommand(yamldata, toolbase.PATH, toolbase.Optional, toolbase.VALUE, toolbase.COMMAND, toolbase.FileName)
-				if err != nil {
-					outputLabel.SetText("Error: " + err.Error())
-				} else {
-					outputLabel.SetText("Running: " + toolbase.Name)
-				}
-			})
-			buttons = append(buttons, btn)
-			allButtons = append(allButtons, btn) // 保证 allButtons 包含所有按钮的引用
-		}
-
-		catContainer := container.NewVBox(labelContainer)
-		catContainer.Add(container.NewGridWrap(fyne.NewSize(160, 30), buttons...))
-		canvasObjects = append(canvasObjects, catContainer)
-		categoryContainers = append(categoryContainers, catContainer)
-	}
-
-	mainContent := container.NewVBox(canvasObjects...)
+	mainContent := container.NewVBox(categoryContainers...)
 	scrollableContent := container.NewScroll(mainContent)
 	scrollableContent.SetMinSize(fyne.NewSize(850, 650))
 
-	backgroundWithContent := container.NewMax(background, scrollableContent)
-	// 创建搜索框
 	searchEntry := widget.NewEntry()
 	searchEntry.SetPlaceHolder("搜索工具...")
+	searchEntry.Resize(fyne.NewSize(600, 40))
 
-	// 搜索框内容变化时过滤显示的按钮
 	searchEntry.OnChanged = func(s string) {
 		if s == "" {
+			updateToolContainers(outputLabel, &categoryContainers, &allContainers)
 			scrollableContent.Content = container.NewVBox(categoryContainers...)
 		} else {
 			filteredObjects := []fyne.CanvasObject{}
 			s = strings.ToLower(s)
-			for _, btn := range allButtons {
+			for _, container := range allContainers {
+				btn := container.Objects[0].(*widget.Button)
 				if strings.Contains(strings.ToLower(btn.Text), s) {
-					filteredObjects = append(filteredObjects, btn)
+					filteredObjects = append(filteredObjects, container)
 				}
 			}
 			if len(filteredObjects) > 0 {
@@ -137,96 +155,299 @@ func main() {
 		scrollableContent.Refresh()
 	}
 
-	// 创建取消搜索按钮
 	clearButton := widget.NewButton("取消搜索", func() {
 		searchEntry.SetText("")
+		updateToolContainers(outputLabel, &categoryContainers, &allContainers)
 		scrollableContent.Content = container.NewVBox(categoryContainers...)
 		scrollableContent.Refresh()
 	})
 
-	searchContainer := container.NewBorder(nil, nil, nil, clearButton, searchEntry)
+	addButton := widget.NewButton("添加工具", func() {
+		nameEntry := widget.NewEntry()
+		nameEntry.SetPlaceHolder("工具名称")
+		pathEntry := widget.NewEntry()
+		pathEntry.SetPlaceHolder("需要在app包内resources文件夹内")
+		filenameEntry := widget.NewEntry()
+		filenameEntry.SetPlaceHolder("文件名")
+		valueEntry := widget.NewEntry()
+		valueEntry.SetPlaceHolder("Java8/Java11/Open/openterm")
+		commandEntry := widget.NewEntry()
+		commandEntry.SetPlaceHolder("命令")
+		optionalEntry := widget.NewEntry()
+		optionalEntry.SetPlaceHolder("可选参数")
 
-	content := container.NewBorder(searchContainer, outputLabel, nil, nil, backgroundWithContent)
+		categoryNames := make([]string, len(categories.Category))
+		for i, category := range categories.Category {
+			categoryNames[i] = category.Name
+		}
+		categorySelect := widget.NewSelect(categoryNames, nil)
+
+		form := container.NewVBox(
+			widget.NewForm(
+				widget.NewFormItem("工具名称", nameEntry),
+				widget.NewFormItem("工具路径", pathEntry),
+				widget.NewFormItem("执行文件名", filenameEntry),
+				widget.NewFormItem("运行方式", valueEntry),
+				widget.NewFormItem("命令", commandEntry),
+				widget.NewFormItem("可选参数", optionalEntry),
+				widget.NewFormItem("类别", categorySelect),
+			),
+		)
+
+		var dialogWindow dialog.Dialog
+		dialogWindow = dialog.NewCustomConfirm("添加新工具", "提交", "取消", form, func(confirm bool) {
+			if confirm {
+				newTool := Tool{
+					Name:     nameEntry.Text,
+					Path:     pathEntry.Text,
+					FileName: filenameEntry.Text,
+					Value:    valueEntry.Text,
+					Command:  commandEntry.Text,
+					Optional: optionalEntry.Text,
+				}
+				for i, category := range categories.Category {
+					if category.Name == categorySelect.Selected {
+						categories.Category[i].Tool = append(categories.Category[i].Tool, newTool)
+						break
+					}
+				}
+				updateYAMLFile()
+				updateToolContainers(outputLabel, &categoryContainers, &allContainers)
+				scrollableContent.Content = container.NewVBox(categoryContainers...)
+				scrollableContent.Refresh()
+			}
+		}, myWindow)
+
+		dialogWindow.Resize(fyne.NewSize(400, 400))
+		dialogWindow.Show()
+	})
+
+	removeButton := widget.NewButton("删除工具", func() {
+		categoryNames := make([]string, len(categories.Category))
+		for i, category := range categories.Category {
+			categoryNames[i] = category.Name
+		}
+		categorySelect := widget.NewSelect(categoryNames, nil)
+		toolSelect := widget.NewSelect([]string{}, nil)
+
+		categorySelect.OnChanged = func(s string) {
+			var toolNames []string
+			for _, category := range categories.Category {
+				if category.Name == s {
+					for _, tool := range category.Tool {
+						toolNames = append(toolNames, tool.Name)
+					}
+					break
+				}
+			}
+			toolSelect.Options = toolNames
+			toolSelect.Refresh()
+		}
+
+		form := container.NewVBox(
+			widget.NewForm(
+				widget.NewFormItem("类别", categorySelect),
+				widget.NewFormItem("工具名称", toolSelect),
+			),
+		)
+
+		var dialogWindow dialog.Dialog
+		dialogWindow = dialog.NewCustomConfirm("删除工具", "提交", "取消", form, func(confirm bool) {
+			if confirm {
+				toolName := toolSelect.Selected
+				var found bool
+				for i, category := range categories.Category {
+					if category.Name == categorySelect.Selected {
+						for j, tool := range category.Tool {
+							if tool.Name == toolName {
+								categories.Category[i].Tool = append(categories.Category[i].Tool[:j], categories.Category[i].Tool[j+1:]...)
+								found = true
+								break
+							}
+						}
+						break
+					}
+				}
+				if found {
+					updateYAMLFile()
+					updateToolContainers(outputLabel, &categoryContainers, &allContainers)
+					scrollableContent.Content = container.NewVBox(categoryContainers...)
+					scrollableContent.Refresh()
+					outputLabel.SetText("Removed: " + toolName)
+				} else {
+					outputLabel.SetText("Tool not found: " + toolName)
+				}
+			}
+		}, myWindow)
+
+		dialogWindow.Resize(fyne.NewSize(400, 200))
+		dialogWindow.Show()
+	})
+
+	searchAndAddContainer := container.NewBorder(nil, nil, nil, container.NewHBox(clearButton, addButton, removeButton), searchEntry)
+
+	var background *canvas.RadialGradient
+	if currentTheme.isDarkMode {
+		background = canvas.NewRadialGradient(color.RGBA{R: 25, G: 25, B: 25, A: 255}, color.RGBA{R: 0, G: 0, B: 0, A: 255})
+	} else {
+		background = canvas.NewRadialGradient(color.RGBA{R: 255, G: 255, B: 255, A: 255}, color.RGBA{R: 255, G: 255, B: 255, A: 255})
+	}
+
+	content := container.NewMax(background, container.NewBorder(searchAndAddContainer, outputLabel, nil, nil, scrollableContent))
 
 	myWindow.SetContent(content)
-	myWindow.Resize(fyne.NewSize(800, 600))
+	myWindow.Resize(fyne.NewSize(800, 750))
 	myWindow.CenterOnScreen()
+
 	myWindow.ShowAndRun()
 }
 
+func createToolContainer(toolbase Tool, outputLabel *widget.Label) *fyne.Container {
+	button := widget.NewButton(toolbase.Name, func() {
+		err := ExecuteCommand(toolbase.Path, toolbase.Optional, toolbase.Value, toolbase.FileName)
+		if err != nil {
+			outputLabel.SetText("Error: " + err.Error())
+		} else {
+			outputLabel.SetText("Running: " + toolbase.Name)
+		}
+	})
+
+	toolContainer := container.NewVBox(button)
+
+	return toolContainer
+}
+
+func updateToolContainers(outputLabel *widget.Label, categoryContainers *[]fyne.CanvasObject, allContainers *[]*fyne.Container) {
+	*categoryContainers = nil
+	*allContainers = nil
+
+	for _, category := range categories.Category {
+		label := widget.NewLabel(category.Name)
+		labelContainer := container.NewMax(label)
+		var buttons []fyne.CanvasObject
+
+		for _, toolbase := range category.Tool {
+			toolbase := toolbase
+			container := createToolContainer(toolbase, outputLabel)
+			buttons = append(buttons, container)
+			*allContainers = append(*allContainers, container)
+		}
+
+		catContainer := container.NewVBox(labelContainer)
+		gridContainer := container.NewGridWrap(fyne.NewSize(200, 40), buttons...)
+		catContainer.Add(gridContainer)
+		*categoryContainers = append(*categoryContainers, catContainer)
+	}
+}
+
 func ReadYAMLFile() ([]byte, error) {
-	currentDir, err := os.Getwd()
+	if cachedYAMLData != nil {
+		return cachedYAMLData, nil
+	}
 	filename := "tool.yml"
 	data, err := ioutil.ReadFile(filename)
-	WriteErrorToFile(filename)
-	WriteErrorToFile(currentDir)
 	if err != nil {
-		WriteErrorToFile(err.Error())
+		WriteErrorToFile("Error reading file", filename, err)
 		return nil, fmt.Errorf("读取文件出错: %v", err)
 	}
+	cachedYAMLData = data
 	return data, nil
 }
 
-func WriteErrorToFile(errMsg string) {
-	file, err := os.OpenFile("errorsss.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+func updateYAMLFile() {
+	filename := "tool.yml"
+	data, err := yaml.Marshal(&struct {
+		Categories []Category `yaml:"Categories"`
+		JavaPath   struct {
+			Java8  string `yaml:"Java8"`
+			Java11 string `yaml:"Java11"`
+			Open   string `yaml:"Open"`
+		} `yaml:"javapath"`
+	}{
+		Categories: categories.Category,
+		JavaPath:   config.JavaPath,
+	})
 	if err != nil {
-		fmt.Println("打开错误日志文件失败:", err)
+		WriteErrorToFile("YAML Marshal error", filename, err)
 		return
 	}
-	defer file.Close()
 
-	_, err = file.WriteString(errMsg + "\n")
+	content := "# Java 8\n" +
+		"# 路径：resources/java8/bin/java\n" +
+		"# 这个路径指向Java 8的可执行文件，适用于需要Java 8环境的应用。\n" +
+		"# Java 11\n" +
+		"# 路径：resources/java11/bin/java\n" +
+		"# 这个路径指向Java 11的可执行文件，适用于需要Java 11环境的应用。\n" +
+		"# 打开方式\n" +
+		"# 命令：open\n" +
+		"# 该命令用于打开或执行文件，具体依赖于操作系统的配置。\n" +
+		string(data)
+
+	err = ioutil.WriteFile(filename, []byte(content), 0644)
 	if err != nil {
-		fmt.Println("写入错误日志文件失败:", err)
+		WriteErrorToFile("WriteFile error", filename, err)
+	}
+	cachedYAMLData = nil // Invalidate the cache
+}
+
+func WriteErrorToFile(msg, filename string, err error) {
+	logFile, fileErr := os.OpenFile("errors.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if fileErr != nil {
+		fmt.Println("Failed to open error log file:", fileErr)
 		return
+	}
+	defer logFile.Close()
+
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	logMsg := fmt.Sprintf("%s | ERROR | %s | %s: %v\n", timestamp, filename, msg, err)
+	_, fileErr = logFile.WriteString(logMsg)
+	if fileErr != nil {
+		fmt.Println("Failed to write to error log file:", fileErr)
 	}
 }
 
-func ExecuteCommand(yamldata []byte, path, optional, command, value string, filename string) error {
-	var config Config
-	if err := yaml.Unmarshal(yamldata, &config); err != nil {
-		WriteErrorToFile("YAML Unmarshal error: " + err.Error())
+func ExecuteCommand(path, optional, value, filename string) error {
+	yamldata, err := ReadYAMLFile()
+	if err != nil {
 		return err
 	}
 
-	// 获取应用的当前工作目录
+	if err := yaml.Unmarshal(yamldata, &config); err != nil {
+		WriteErrorToFile("YAML Unmarshal error", "config", err)
+		return err
+	}
+
 	currentDir, err := os.Getwd()
 	if err != nil {
-		WriteErrorToFile("Get working directory error: " + err.Error())
+		WriteErrorToFile("Get working directory error", "currentDir", err)
 		return err
 	}
 
 	var execCommand string
-	// 根据 command 参数决定使用 Java8 还是 Java11 路径，或者是执行 Open 命令
-	switch command {
+	switch value {
 	case "Java8", "Java11":
-		javaPath := filepath.Join(currentDir, config.Java_path.Java8)
-		if command == "Java11" {
-			javaPath = filepath.Join(currentDir, config.Java_path.Java11)
+		javaPath := filepath.Join(currentDir, config.JavaPath.Java8)
+		if value == "Java11" {
+			javaPath = filepath.Join(currentDir, config.JavaPath.Java11)
 		}
-		execCommand = fmt.Sprintf("cd %s && %s %s %s %s", path, javaPath, optional, value, filename)
+		execCommand = fmt.Sprintf("cd %s && %s %s -jar %s", path, javaPath, optional, filename)
 	case "Open":
 		execCommand = fmt.Sprintf("cd %s && open %s", path, filename)
 	case "openterm":
 		return openTerminal(path)
 	default:
-		return fmt.Errorf("unsupported command: %s", command)
+		return fmt.Errorf("unsupported value: %s", value)
 	}
 
 	fmt.Println("Executing command:", execCommand)
 
-	var cmd *exec.Cmd
-
-	cmd = exec.Command("sh", "-c", execCommand)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true, // 在 UNIX-like 系统中创建一个新的进程组
-	}
+	cmd := exec.Command("sh", "-c", execCommand)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Start(); err != nil {
-		WriteErrorToFile("Command execution failed: " + err.Error())
+		WriteErrorToFile("Command execution failed", execCommand, err)
 		return err
 	}
 	return nil
@@ -234,25 +455,41 @@ func ExecuteCommand(yamldata []byte, path, optional, command, value string, file
 
 func openTerminal(dir string) error {
 	var cmd *exec.Cmd
-	currentDir, err := os.Getwd()
-	if err != nil {
-		return err
-	}
 	switch runtime.GOOS {
 	case "darwin":
-		full_path := filepath.Join(currentDir, dir)
-		script := fmt.Sprintf("tell application \"Terminal\" to do script \"cd %s; ls --color=always\"", full_path)
-		cmd = exec.Command("osascript", "-e", script)
-	//case "windows":
-	//	cmd = exec.Command("cmd", "/C", "start", "cmd", "/k", "cd", dir)
+		currentDir, err := os.Getwd()
+		if err != nil {
+			WriteErrorToFile("Get working directory error", dir, err)
+			return err
+		}
+		fullPath := filepath.Join(currentDir, dir)
+		itermPath := "/Applications/iTerm.app"
+
+		if _, err := os.Stat(itermPath); err == nil {
+			script := fmt.Sprintf(`tell application "iTerm"
+                create window with default profile
+                tell current session of current window
+                    write text "cd %s; ls --color=always"
+                end tell
+            end tell`, fullPath)
+			cmd = exec.Command("osascript", "-e", script)
+		} else {
+			script := fmt.Sprintf(`tell application "Terminal"
+                do script "cd %s; ls --color=always"
+            end tell`, fullPath)
+			cmd = exec.Command("osascript", "-e", script)
+		}
 	default:
-		return fmt.Errorf("unsupported platform")
+		err := fmt.Errorf("unsupported platform")
+		WriteErrorToFile("Open terminal error", dir, err)
+		return err
 	}
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err = cmd.Start()
+	err := cmd.Start()
 	if err != nil {
+		WriteErrorToFile("Failed to open terminal", dir, err)
 		return fmt.Errorf("failed to open terminal: %w", err)
 	}
 	return nil
